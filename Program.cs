c17671fa41e1537c1b877e9820f475b06e69f109
Regiane_de_Alcântara <any_any_2011@hotmail.com>
2024-03-02 21:39:10 -0300

    InicioProjeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Fazer um programa que calcule a media de 03 notas do aluno = String
+//Solicitar ao usuario
+//nome de um aluno = String
+//três notas que ele obteve nos três trimestres do ano = double
+//primeiro trimestre vale 30
+//segundo e terceiro valem 35 cada
+//Mostrar em tela nota final do aluno no ano.
+//Informar se APROVADO ou REPROVADO 
+//Se  reprovado mostra uma mensagem informando
+//quantos pontos o aluno deveria obter para ser aprovado
+//Considerar nota para aprovação 60 pontos = double
+//Sem orientação a objetos
